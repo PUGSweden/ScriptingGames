76a221cb541cbb19bd4b1712f99d97f3586caf12
Simon Wåhlin <simon.wahlin@gmail.com>
2016-01-22 13:36:11 +0100

    Added mock InModuleScope

--- January2016/uptime/Uptime.Tests.ps1
+++ January2016/uptime/Uptime.Tests.ps1
@@ -1,24 +1,27 @@
 ﻿Import-Module $PSScriptRoot -Force
 
 Describe "Uptime" {
-    Context "CimSessionError" {
-        Mock -CommandName New-CIMSession -MockWith {
-            Write-Error -Category PermissionDenied -Message 'Access is denied'
-        }
-        It "Returns OFFLINE status for nonexisting computer" {
-            $Uptime = Get-Uptime -ComputerName NONEXISTING
-            $Uptime.Status | Should be 'OFFLINE'
-        }
-
-        It "Returns OK status for existing computer" {
-            $Uptime = Get-Uptime -ComputerName $env:COMPUTERNAME
-            $Uptime.Status | Should be 'OK'
-        }
-
-        It "Returns ERROR status for accessdenied computer" {
-            $Uptime = Get-Uptime -ComputerName ERRORCOMPUTER
-            $Uptime.Status | Should be 'ERROR'
+    InModuleScope -ModuleName Uptime {
+        Mock New-CimSession -ParameterFilter {$ComputerName -eq 'ERRORCOMPUTER'} {
+                Write-Error -Category PermissionDenied -Message 'Access is denied'
         }
     }
+    
+    It "Returns OFFLINE status for nonexisting computer" {
+        $Uptime = Get-Uptime -ComputerName NONEXISTING
+        $Uptime.Status | Should be 'OFFLINE'
+    }
 
+    It "Returns OK status for existing computer" {
+        $Uptime = Get-Uptime -ComputerName $env:COMPUTERNAME
+        $Uptime.Status | Should be 'OK'
+    }
+
+    It "Returns ERROR status for accessdenied computer" {
+        $Uptime = Get-Uptime -ComputerName ERRORCOMPUTER
+        $Uptime.Status | Should be 'ERROR'
+        $Uptime.StartTime | Should be $null
+        $Uptime.Uptime | Should be $null
+            
+    }
 }
